901dd632cdd8fd8bcf2483f3668c6856b140a0ff
Italo Vinicius <107862087+italo-vinicius@users.noreply.github.com>
2023-03-22 18:07:25 -0300

    Update web.php
    Add doc para as rotas

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+//Rotas para Login
 Route::resource('usuario', UserController::class)->names('user')->parameters(['usuario' => 'user']);
 Route::get('getUser', [UserController::class, 'getUser'])->name('getUser'); //Autentica o usuário e redireciona pra home
 
+
+//Rotas da Home
 Route::get('home/{id}', [HomeController::class, 'index'])->name('homePage');
 Route::post('home/{id}/createPost', [HomeController::class, 'createPost'])->name('createPost');
 Route::get('home/{id}/timeline', [HomeController::class, 'showTimeline'])->name('timeline');
